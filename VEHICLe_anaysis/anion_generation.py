f588d030608610791590b1e21af22c8f1bc03529
JoeHeeley96 <yv19151@bristol.ac.uk>
2021-04-15 17:21:26 +0100

    bromine and anions now work, need to test in bulk but write a workflow then you're laughing

--- VEHICLe_anaysis/anion_generation.py
+++ VEHICLe_anaysis/anion_generation.py
@@ -1,4 +1,8 @@
-
+import glob
+from rdkit import Chem
+from rdkit.Chem import AllChem
+import numpy as np
+from datetime import date
 
 def anion_from_com(comfilename):
     ### If we rewrite this we can make sure that only H's bound to carbons are stripped! ###
@@ -29,5 +33,58 @@ def anion_from_com(comfilename):
                                 elif line != k:
                                     print(line.strip('\n'), file=p)
 
-def anions_from_smiles(dataframe, neutral_comfilename):
-    print('not written yet. Use getneighbors() to find Hs only attached to Cs and the use rdgid to find comfile and to strip out those coords')
+def anions_from_smiles(dataframe):
+
+    regid = dataframe['Regid']
+
+    for l in regid:
+        H_index_list = []
+        C_index_list = []
+        C_numbering = []
+        comfiles = glob.glob('neutral_comfiles/' + str(l) + '_*')
+        index_file = glob.glob('txt_files/' + str(l) + '_indexing.txt')
+        row = dataframe[dataframe.Regid == l]
+        smiles = row.Smiles.item()
+
+        mol = Chem.MolFromSmiles(smiles)
+        molH = Chem.AddHs(mol)
+        molc = AllChem.Compute2DCoords(molH)
+        a = molH.GetAtoms()
+
+        for j, atoms in enumerate(a):
+            b = atoms.GetAtomicNum()
+            d = atoms.GetIdx()
+            if b == 1:
+                e = atoms.GetNeighbors()
+                for j, k in enumerate(e):
+                    if k.GetAtomicNum() == 6:
+                        if str(k.GetHybridization()) == 'SP2':
+                            H_index_list.append(d)
+                            C_index = k.GetIdx()
+                            C_index_list.append(C_index)
+
+        for x in range(len(C_index_list)):
+            C_numbering.append(x + 1)
+
+        match_Cnum_Cindex = zip(H_index_list, C_numbering)
+
+        for file in comfiles:
+            todays_date = str(date.today())
+            split = file.split('\\')
+            name = split[1].split('_')
+            for m in index_file:
+                for n, o in match_Cnum_Cindex:
+                    with open(m) as p:
+                        for line in p:
+                            comindex = line[2]
+                            if str(n) == comindex:
+                                xyz=line[4:]
+                                anion_comfilename = 'TEST/' + name[0] + '_' + str(o) + 'anion_' + todays_date + '_' + name[2] + '-' + name[3] + '_opt.com'
+                                bromine_chkfilename = name[0] + '_' + str(o) + 'anion_' + todays_date + '_' + name[ 2] + '-' + name[3] + '_opt.chk'
+                                with open(anion_comfilename, 'w') as q:
+                                    with open(file) as f:
+                                        for comline in f:
+                                            if str(xyz) in comline:
+                                                pass
+                                            else:
+                                                print(comline.strip('\n'), file=q)
--- VEHICLe_anaysis/bromine_generation.py
+++ VEHICLe_anaysis/bromine_generation.py
@@ -1,6 +1,9 @@
 from xyzfile_generation import xyz_from_com
 from xyz2mol import xyz2mol
 from datetime import date
+import glob
+from rdkit import Chem
+from rdkit.Chem import AllChem
 
 def bromine_check(bromine_comfilename):
     xyz_from_com(bromine_comfilename)
@@ -90,3 +93,73 @@ def bromine_comfiles_from_xyz(xyzfilename):
 
                             else:
                                 print(line.strip('\n'), file=p)
+def bromines_from_smiles(dataframe):
+    print('turn me into a module you fat cow')
+    regid = VEHICLe_1['Regid']
+
+    for l in regid:
+        C_index_list = []
+        C_numbering = []
+        comfiles = glob.glob('neutral_comfiles/' + str(l) + '_*')
+        index_file = glob.glob('txt_files/' + str(l) + '_indexing.txt')
+        row = VEHICLe[VEHICLe.Regid == l]
+        smiles = row.Smiles.item()
+
+        mol = Chem.MolFromSmiles(smiles)
+        molH = Chem.AddHs(mol)
+        molc = AllChem.Compute2DCoords(molH)
+        a = molH.GetAtoms()
+
+        for j, atoms in enumerate(a):
+            b = atoms.GetAtomicNum()
+            d = atoms.GetIdx()
+            if b == 1:
+                e = atoms.GetNeighbors()
+                for j, k in enumerate(e):
+                    if k.GetAtomicNum() == 6:
+                        if str(k.GetHybridization()) == 'SP2':
+                            C_index = k.GetIdx()
+                            C_index_list.append(C_index)
+
+        for x in range(len(C_index_list)):
+            C_numbering.append(x + 1)
+
+        match_Cnum_Cindex = zip(C_index_list, C_numbering)
+        ### Use same numbering systen in anion generation ###
+
+        for file in comfiles:
+            todays_date = str(date.today())
+            split = file.split('\\')
+            name = split[1].split('_')
+            for m in index_file:
+                for n, o in match_Cnum_Cindex:
+                    with open(m) as p:
+                        for line in p:
+                            comindex = line[2]
+                            if str(n) == comindex:
+                                bromine_comfilename = 'bromine_comfiles/' + name[0] + '_' + str(o) + 'bromine_' + todays_date + '_' + name[2] + '-' + name[3] + '_opt.com'
+                                bromine_chkfilename = name[0] + '_' + str(o) + 'bromine_' + todays_date + '_' + name[
+                                    2] + '-' + name[3] + '_opt.chk'
+                                split = line.split(' ')
+                                Br_xcoord = float(split[2])
+                                Br_ycoord = float(split[3])
+                                Br_zcoord = float(split[4])
+                                Br_coords = (str(Br_xcoord) + ' ' + str(Br_ycoord) + ' ' + str(Br_zcoord))
+
+                            with open(bromine_comfilename, 'w') as q:
+                                with open(file) as f:
+                                    for comline in f:
+
+                                        if comline.startswith('%chk='):
+                                            print('%chk=' + bromine_chkfilename, file=q)
+
+                                        elif comline == '0 1\n':
+                                            print('1 1', file=q)
+
+                                        elif Br_coords in comline:
+                                            print(comline.strip('\n'), file=q)
+                                            print('Br', Br_xcoord, Br_ycoord, (Br_zcoord + 2), file=q)
+
+
+                                        else:
+                                            print(comline.strip('\n'), file=q)
